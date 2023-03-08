9405c3dfbd9680e465104a74f54ee355bd0306e0
magbha <abdelghanibha@gmail.com>
2023-03-08 02:40:54 +0000

    a

--- src/Instance/axios.js
+++ src/Instance/axios.js
@@ -6,6 +6,7 @@ export default axios.create({
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     },
-    withCredentials: true
+    withCredentials: false
 })
