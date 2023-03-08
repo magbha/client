d15ccb0ee61f47e3b38415f730c9d8b0d4708553
magbha <abdelghanibha@gmail.com>
2023-03-08 03:57:39 +0000

    abd

--- src/Instance/axios.js
+++ src/Instance/axios.js
@@ -3,5 +3,9 @@ import axios from "axios"
 
 export default axios.create({
     baseURL: "https://stroom-api.onrender.com",
-    
+    headers: {
+        'Access-Control-Allow-Origin': 'https://st-room-inventory.onrender.com',
+
+    },
+
 })
